5792499488154befb0a65cf80643906465874cd5
Roger Earl <rogerearl@me.com>
2023-02-13 21:05:15 -0800

    Added style to div

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
 function App() {
+  const styledText = {
+    color: "red",
+    fontSize: "25px",
+    textAlign: "center",
+    margin: "40px",
+    border: "1px solid red",
+    backgroundColor: "blue"
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div style={styledText}>
+      <p>Hello World</p>
     </div>
   );
 }
